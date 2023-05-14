8587415545f176da47d0a520027a142b9f8e5714
Nahi-shady <116443749+Nahi-shady@users.noreply.github.com>
2023-05-14 11:31:23 +0300

    added comments to Domino.py

--- Math/Domino piling.py
+++ Math/Domino piling.py
@@ -1,4 +1,4 @@
 def domino(M,N):
-    return M*N//2
+    return M*N//2          # Devide the area of the rectanle by the area of 1 Domino.
 
-print(domino(int(input()), int(input())))
+print(domino(int(input()), int(input())))     # Take two inputs from cmd and convert them into integer
