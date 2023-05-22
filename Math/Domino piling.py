672fba4c9e44f45298ad39cf79ec550640133264
Nahom Zewdu <nahiyo86@gmail.com>
2023-05-22 20:23:15 +0300

    correction for Domino pilling solution

--- Math/Domino piling.py
+++ Math/Domino piling.py
@@ -1,4 +1,2 @@
-def domino(M,N):
-    return M*N//2          # Devide the area of the rectanle by the area of 1 Domino.
-
-print(domino(int(input()), int(input())))     # Take two inputs from cmd and convert them into integer
+m, n = map(int, input().split())
+print((m*n)//2)
